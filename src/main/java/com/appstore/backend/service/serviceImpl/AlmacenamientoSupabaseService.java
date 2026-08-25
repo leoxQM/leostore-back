6c91e43cfe275643821e2539060ef1f8aba37c8f
leoQM <theleox12@gmail.com>
2026-08-25 18:26:46 -0500

    add anotacion primary

--- src/main/java/com/appstore/backend/service/serviceImpl/AlmacenamientoSupabaseService.java
+++ src/main/java/com/appstore/backend/service/serviceImpl/AlmacenamientoSupabaseService.java
@@ -2,6 +2,7 @@ package com.appstore.backend.service.serviceImpl;
 
 import com.appstore.backend.service.AlmacenamientoService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Service
+@Primary
 @Profile("prod")   // ← solo se activa cuando el perfil "prod" está activo
 public class AlmacenamientoSupabaseService implements AlmacenamientoService {
 
